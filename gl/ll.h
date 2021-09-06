c28c8ef634c82a88ddc04e5607707c0ed1be0f8c
Travis West <travis@traviswest.ca>
2021-09-06 16:12:22 +0200

    constructor for high-level program

--- gl/ll.h
+++ gl/ll.h
@@ -5,8 +5,15 @@
 /* low level GL interface
  *
  * This level is meant to serve as a very thin wrapper over OpenGL ES 3.0
- * providing a direct one-to-one interface to the latter, with type safety and
- * debug-mode-only automatic error checking.
+ * providing a reasonably one-to-one interface to the latter, with type safety,
+ * lifetime management, and automatic error checking that is disabled in
+ * release-mode builds.
  */
 
 #include "ll/shader.h"
+#include "ll/program.h"
+#include "ll/buffer.h"
+#include "ll/vertex_array.h"
+#include "ll/attributes.h"
+#include "ll/attribute_manifest.h"
+#include "ll/error.h"
