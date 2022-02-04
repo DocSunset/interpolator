e3bab886a91b72bb89c04cc7fc938366618497c6
Travis West <travis@traviswest.ca>
2022-02-04 11:37:55 +0100

    restore auditioning

--- systems/interpolator.cpp
+++ systems/interpolator.cpp
@@ -1,43 +1,60 @@
 #include "interpolator.h"
-#include <iostream>
+
+#include "entt/entity/entity.hpp"
+
 #include "components/demo.h"
 #include "components/paint_flag.h"
 #include "components/mouse_button.h"
 #include "components/mouse_motion.h"
 #include "components/draggable.h"
 #include "components/knob.h"
+#include "components/cursor.h"
+
 #include "systems/common/interpolator.h"
-#include "entt/entity/entity.hpp"
+#include "systems/common/vis.h"
 
 namespace System
 {
     struct Interpolator::Implementation
     {
-        bool pressed = false;
         bool hid_override = false;
         Component::Demo::Source cache = Component::Demo::Source::Zero();
 
-        void hid_override_query(entt::registry& registry)
+        void audition(entt::registry& registry, entt::entity entity)
         {
-            hid_override = true;
-            //// if grabbed knob, play a demo
-            //auto grabbed_knob = registry.view<Component::Knob, Component::Grabbed>();
-            //for (auto && [entity, knob] : grabbed_knob.each())
-            //{
-            //    // get first selected demo
-            //    entt::entity first_demo = entt::null;
-            //    auto first_demo_view = registry.view<Component::Selected, Component::Demo>();
-            //    for (auto e : first_demo_view) { first_demo = e; break; }
-            //    if (first_demo == entt::null)
-            //    {
-            //        return; // don't edit context if dragging knob with no sel
-            //    }
+            const auto& d = registry.get<Component::Demo::Destination>(entity);
+            registry.set<Component::Demo::Destination>(d);
+            registry.ctx<Component::PaintFlag>().set();
+        }
 
-            //    // play first selected demo's parameters
-            //    auto s = registry.get<Component::FMSynthParameters>(first_demo);
-            //    registry.set<Component::FMSynthParameters>(s);
-            //    return;
-            //}
+        void audition(entt::registry& registry, const Component::Position& position)
+        {
+            auto source = position_to_source(registry, position);
+            auto& destination = registry.ctx<Component::Demo::Destination>();
+            destination = query(registry, source);
+            registry.ctx<Component::PaintFlag>().set();
+        }
+
+        void hid_override_query(entt::registry& registry, const Component::Position& position)
+        {
+            auto grabbed_knob = registry.view<Component::Knob, Component::Grabbed>();
+            for (auto && [entity, knob] : grabbed_knob.each())
+            {
+                // get first selected demo
+                auto selected_demo = registry.view<Component::Selected, Component::Demo>();
+                for (auto entity : selected_demo) return audition(registry, entity);
+                break;
+            }
+
+            // play first grabbed demo; should really be closest grabbed demo...
+            auto grabbed_demo = registry.view<Component::Grabbed, Component::Demo>();
+            for (auto entity : grabbed_demo) return audition(registry, entity);
+
+            auto grabbed_cursor = registry.view<Component::Grabbed, Component::Cursor>();
+            for (auto entity : grabbed_cursor)
+                return audition(registry, registry.get<Component::Position>(entity));
+
+            audition(registry, position);
         }
 
         void normal_query(entt::registry& registry)
@@ -63,22 +80,24 @@ namespace System
         void on_mouse_button(entt::registry& registry, entt::registry::entity_type entity)
         {
             auto button = registry.get<Component::LeftMouseButton>(entity);
-            if (button.consumed) return;
-            pressed = button.pressed;
-            if (pressed) hid_override_query(registry);
+            if (not button.consumed && button.pressed)
+            {
+                hid_override = true;
+                hid_override_query(registry, button.down_position);
+            }
+            else hid_override = false;
         }
 
         void on_mouse_motion(entt::registry& registry, entt::registry::entity_type entity)
         {
-            if (not pressed) return;
+            if (not hid_override) return;
             auto motion = registry.get<Component::MouseMotion>(entity);
-            hid_override_query(registry);
+            hid_override_query(registry, motion.position);
         }
 
         void run(entt::registry& registry)
         {
-            if (hid_override) hid_override = false;
-            else normal_query(registry);
+            if (!hid_override) normal_query(registry);
         }
     };
 
