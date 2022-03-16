682ee4ed852045842be61edea01b90f560b3e62c
Travis West <travis@traviswest.ca>
2022-03-16 10:30:17 +0100

    drop all hope of emscripten build
    libmapper isn't compatible anyways. If that ever changes, this is the
    commit to look at.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,3 @@
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#endif
-
 #include "app.h"
 #include "systems/platform.h"
 #include "systems/draggable.h"
@@ -22,13 +18,6 @@
 #include "systems/pca.h"
 #include "systems/demo_dest_knobs.h"
 #include "systems/manual_color_knobs.h"
-
-#ifdef __EMSCRIPTEN__
-void loop(void * app)
-{
-    static_cast<App*>(app)->loop();
-}
-#endif
 
 int main(int argc, char ** argv)
 {
@@ -54,14 +43,10 @@ int main(int argc, char ** argv)
         , System::Libmapper // syncs destination signals with registry
         >(argc, argv);
 
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop_arg(&loop, &app, -1, 1);
-#else
     while (not app.ready_to_quit())
     {
         app.loop();
     }
-#endif
 
     return 0;
 }
